77e233ed070cbbc566a2a73ffba7d94efe09c32d
Harshitdot1106 <anand.harshit2005@gmail.com>
2025-01-29 23:31:45 +0530

    frontend

--- Backend/Routes/maps.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { authUser } from '../middleware/authtoken.js';
-import {getCoordinates,getDistanceTime,getSuggestion} from '../controller/maps.controller.js'
-const router=express.Router();
-router.get('/get-coordinates',authUser,getCoordinates);
-router.get('/get-distance-time',authUser,getDistanceTime);
-router.get('/get-suggestions',authUser,getSuggestion)
-export default router
